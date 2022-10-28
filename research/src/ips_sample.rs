c1d86771d6e0004da1427eb76bb1a9b94d13c698
B1TC0R3 <github@b1tc0r3.com>
2022-10-28 15:18:57 +0200

    Add zbus to research dependencies
    Fix target ignore

---
+++ research/src/ips_sample.rs
@@ -0,0 +1,8 @@
+/*
+ * This module is spposed to show how to use zbus to communicate with dbus.
+ */
+
+pub fn function() {
+
+    ()
+}
--- research/src/main.rs
+++ research/src/main.rs
@@ -4,8 +4,10 @@
  * This way, multiple things can be tested with the same main file.
  */
 
+//IMPORTANT: Comment out unused `mod` statements to avoid lengthy build times
+
 //Module imports
-mod gdb_practice;
+//mod gdb_practice;
 
 fn main() {
     //gdb_practice::start_gdb_practice();
