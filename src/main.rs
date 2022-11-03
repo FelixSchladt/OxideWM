abbc13b0b210075bbf01442dae1f798c77f5932b
B1TC0R3 <github@b1tc0r3.com>
2022-11-03 15:50:11 +0100

    Connection to X11

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+//use x11rb::connection::Connection;
+use std::error::Error;
+use log::info;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let (connection, screen_index) = x11rb::connect(None)?;
+    println!("Connection successful (Screen: {})", screen_index);
+
+    Ok(())
 }
