fc01da927569dba44cb586957e43db9ed13c63a2
B1TC0R3 <github@b1tc0r3.com>
2022-11-03 16:54:00 +0100

    Export example window to secondary file

---
+++ src/frames.rs
@@ -0,0 +1,37 @@
+use x11rb::connection::Connection;
+use x11rb::COPY_DEPTH_FROM_PARENT;
+use x11rb::protocol::xproto::*;
+use std::error::Error;
+
+pub fn create_example(
+        x: i16,
+        y: i16,
+        width: u16,
+        height: u16,
+        border_width: u16,
+    ) -> Result<(), Box<dyn Error>>{
+    let (connection, screen_index) = x11rb::connect(None)?;
+    let screen = &connection.setup().roots[screen_index];
+    let window_id = connection.generate_id()?;
+
+    connection.create_window(
+        COPY_DEPTH_FROM_PARENT,
+        window_id,
+        screen.root,
+        x,
+        y,
+        width,
+        height,
+        border_width,
+        WindowClass::INPUT_OUTPUT,
+        0,
+        &CreateWindowAux::new().background_pixel(screen.white_pixel),
+    ).unwrap();
+
+    connection.map_window(window_id).unwrap();
+    connection.flush()?;
+
+    loop {
+        println!("Event: {:?}", connection.wait_for_event().unwrap());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-//use x11rb::connection::Connection;
 use std::error::Error;
-use log::info;
+
+mod frames;
 
 fn main() -> Result<(), Box<dyn Error>>{
-    let (connection, screen_index) = x11rb::connect(None)?;
-    println!("Connection successful (Screen: {})", screen_index);
-
-    Ok(())
+    frames::create_example(
+        0,   //x
+        0,   //y
+        100, //width
+        100, //height
+        5    //border_width
+    )
 }
