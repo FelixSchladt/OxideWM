c0595760d0ec8a32de3d491d94e2a1c9323f1e42
B1TC0R3 <github@b1tc0r3.com>
2022-11-04 11:19:00 +0100

    Fix xephyr etup

--- runx.sh
+++ runx.sh
@@ -1,4 +1,4 @@
-cargo build --example window
+cargo build --example window_with_frame
 
 XEPHYR=$(whereis -b Xephyr | cut -f2 -d ' ')
 xinit ./xinitrc -- $XEPHYR :100 -ac -screen 500x500 -host-cursor 
