a527e60658d9739c7d03f9bc9f4afc4a4fd185a8
B1TC0R3 <github@b1tc0r3.com>
2022-11-04 11:05:57 +0100

    Finalize concept

--- examples/window.rs
+++ examples/window.rs
@@ -12,23 +12,55 @@ pub fn create_example(
     ) -> Result<(), Box<dyn Error>>{
     let (connection, screen_index) = x11rb::connect(None)?;
     let screen = &connection.setup().roots[screen_index];
+    let frame_id = connection.generate_id()?;
+    let titlebar_id = connection.generate_id()?; 
     let window_id = connection.generate_id()?;
+    let titlebar_height: i16 = 10;
 
     connection.create_window(
         COPY_DEPTH_FROM_PARENT,
-        window_id,
+        frame_id,
         screen.root,
         x,
         y,
         width,
         height,
-        border_width,
+        0,
         WindowClass::INPUT_OUTPUT,
         0,
         &CreateWindowAux::new().background_pixel(screen.white_pixel),
-    ).unwrap();
+    )?;
 
-    connection.map_window(window_id).unwrap();
+    connection.create_window(
+        COPY_DEPTH_FROM_PARENT,
+        titlebar_id,
+        frame_id,
+        border_width as i16,
+        border_width as i16,
+        width - 2*border_width,
+        titlebar_height as u16,
+        0,
+        WindowClass::INPUT_OUTPUT,
+        0,
+        &CreateWindowAux::new().background_pixel(screen.black_pixel),
+    )?;
+
+    connection.create_window(
+        COPY_DEPTH_FROM_PARENT,
+        window_id,
+        frame_id,
+        border_width as i16,
+        titlebar_height + border_width as i16,
+        width - 2*border_width,
+        height- titlebar_height as u16 - 2*border_width,
+        0,
+        WindowClass::INPUT_OUTPUT,
+        0,
+        &CreateWindowAux::new().background_pixel(screen.black_pixel),
+    )?;
+
+    connection.map_window(frame_id)?;
+    connection.map_window(window_id)?;
     connection.flush()?;
 
     loop {
@@ -38,10 +70,10 @@ pub fn create_example(
 
 fn main() -> Result<(), Box<dyn Error>> {
     create_example(
-        0,   //x
-        0,   //y
-        100, //width
-        100, //height
-        10,  //border_width
+        20,   //x
+        20,   //y
+        300, //width
+        400, //height
+        5,  //border_width
     )
 }
