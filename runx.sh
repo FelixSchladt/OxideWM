f96653e43839852c247fc81e9bb004bc400136b8
B1TC0R3 <github@b1tc0r3.com>
2022-11-04 14:32:58 +0100

    REconfigure xephyr

--- runx.sh
+++ runx.sh
@@ -1,4 +1,4 @@
 cargo build --example window_with_frame
 
 XEPHYR=$(whereis -b Xephyr | cut -f2 -d ' ')
-xinit ./xinitrc -- $XEPHYR :100 -ac -screen 500x500 -host-cursor 
+xinit ./xinitrc -- $XEPHYR :100 -ac -screen 1000x1000 -host-cursor 
