b992037b67a18b80db53df95c32cdf9b3a1e2683
Philipp <100133402+Philipp6802@users.noreply.github.com>
2022-11-14 10:08:55 +0100

    Removed unnecessary dependencies

--- examples/yaml_rust.rs
+++ examples/yaml_rust.rs
@@ -1,14 +1,54 @@
-use yaml_rust::{YamlLoader, YamlEmitter};
+use serde::{Deserialize, Serialize};
+use serde_yaml::{self};
 
-fn main(){
+#[derive(Debug, Serialize, Deserialize)]
+struct DataSource {
+    name: String,
+    url: String,
+    source_type: String,
+}
 
-let docs = YamlLoader::load_from_str("[1, 2, 3]").unwrap();
-let doc = &docs[0]; // select the first document
-assert_eq!(doc[0].as_i64().unwrap(), 1); // access elements by index
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    update_frequency_sec: u32,
+    num_threads: u32,
+    data_sources: Vec<DataSource>,
+}
+fn main() {
+    let f = std::fs::File::open("./examples/config.yml").expect("Could not open file.");
+    let mut scrape_config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
 
-let mut out_str = String::new();
-let mut emitter = YamlEmitter::new(&mut out_str);
-emitter.dump(doc).unwrap(); // dump the YAML object to a String
+    println!("{:?}", scrape_config);
 
+    println!(
+        "update_frequency_sec: {}",
+        scrape_config.update_frequency_sec
+    );
 
+    for data_source in scrape_config.data_sources.iter() {
+        println!(
+            "name: {}, type: {}, url {}",
+            data_source.name, data_source.source_type, data_source.url
+        );
+    }
+
+    scrape_config.num_threads = 2;
+
+    scrape_config.data_sources.push(DataSource {
+        name: "NYTimes".to_string(),
+        url: "www.nytimes.com".to_string(),
+        source_type: "news".to_string(),
+    });
+    scrape_config.data_sources.push(DataSource {
+        name: "Yahoo News".to_string(),
+        url: "news.yahoo.com".to_string(),
+        source_type: "news".to_string(),
+    });
+
+    let f = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open("./examples/new_config.yml")
+        .expect("Couldn't open file");
+    serde_yaml::to_writer(f, &scrape_config).unwrap();
 }
