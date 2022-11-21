09ded5ac6e27fa2d385884f0e80fbcacd9b81185
Philipp <100133402+Philipp6802@users.noreply.github.com>
2022-11-21 15:10:17 +0100

    Started implementing Structs from ISSUE 28

--- examples/yaml_rust.rs
+++ examples/yaml_rust.rs
@@ -2,53 +2,61 @@ use serde::{Deserialize, Serialize};
 use serde_yaml::{self};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct DataSource {
-    name: String,
-    url: String,
-    source_type: String,
+enum WmCommands {
+    Move,
+    Resize,
+    Quit,
+    Restart,
+    MoveToWorkspace,
+    GoToWorkspace,
+    MoveToWorkspaceAndFollow,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct WmCommand {
+    keys: Vec<char>,
+    command: WmCommands,
+    args: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct UserCommand {
+    keys: Vec<char>,
+    command: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
-    update_frequency_sec: u32,
-    num_threads: u32,
-    data_sources: Vec<DataSource>,
+    cmds: Vec<WmCommand>,
+    user_cmds: Vec<UserCommand>,
+    exec: Vec<String>,
+    exec_always: Vec<String>,
+    border_with: u8,
+    border_color: String,
+    border_focus_color: String,
+    titlebar: bool,
+    gap: u8,
 }
+
+
 fn main() {
     let f = std::fs::File::open("./examples/config.yml").expect("Could not open file.");
-    let mut scrape_config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
+    let scrape_config: WmCommand = serde_yaml::from_reader(f).expect("Could not read values.");
 
-    println!("{:?}", scrape_config);
-
-    println!(
-        "update_frequency_sec: {}",
-        scrape_config.update_frequency_sec
-    );
-
-    for data_source in scrape_config.data_sources.iter() {
-        println!(
-            "name: {}, type: {}, url {}",
-            data_source.name, data_source.source_type, data_source.url
-        );
-    }
-
-    scrape_config.num_threads = 2;
-
-    scrape_config.data_sources.push(DataSource {
-        name: "NYTimes".to_string(),
-        url: "www.nytimes.com".to_string(),
-        source_type: "news".to_string(),
-    });
-    scrape_config.data_sources.push(DataSource {
-        name: "Yahoo News".to_string(),
-        url: "news.yahoo.com".to_string(),
-        source_type: "news".to_string(),
+    scrape_config.keys.push({
+        WmCommand {
+        keys: vec!['M', 'S', 'q'],
+        command: WmCommands::Move,
+        args: "M".to_string(),
+        }
     });
 
-    let f = std::fs::OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open("./examples/new_config.yml")
-        .expect("Couldn't open file");
-    serde_yaml::to_writer(f, &scrape_config).unwrap();
+    // println!("{:?}", scrape_config);
+
+    // let f = std::fs::OpenOptions::new()
+    //     .write(true)
+    //     .create(true)
+    //     .open("./examples/new_config.yml")
+    //     .expect("Couldn't open file");
+    // serde_yaml::to_writer(f, &scrape_config).unwrap();
 }
