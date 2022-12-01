c8c9e2348b7d64734e4cfb0b3e2dcc30a8220e06
Philipp <100133402+Philipp6802@users.noreply.github.com>
2022-12-02 00:56:18 +0100

    Parser Update

--- examples/config_parser.rs
+++ examples/config_parser.rs
@@ -28,23 +28,34 @@ struct UserCommand {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
+
+    #[serde(default = "default_cmds")
     modifier: char,
-    #[serde(default = "default_cmds")]
+
+    #[serde(default = "default_cmds")
     cmds: Vec<WmCommand>,
+
     #[serde(default = "default_user_cmds")]
     user_cmds: Vec<UserCommand>,
+
     #[serde(default = "default_exec")]
     exec: Vec<String>,
+
     #[serde(default = "default_exec_always")]
     exec_always: Vec<String>,
+
     #[serde(default = "default_border_with")]
     border_with: u8,
+    
     #[serde(default = "default_border_color")]
     border_color: String,
+    
     #[serde(default = "default_border_focus_color")]
     border_focus_color: String,
+    
     #[serde(default = "default_titlebar")]
     titlebar: bool,
+    
     #[serde(default = "default_gap")]
     gap: u8,
 }
@@ -57,18 +68,15 @@ fn default_cmds() -> Vec<WmCommand> {
         command: WmCommands::Move, 
         args: "1".to_string()
     }]
-
 }
-
 fn default_user_cmds() -> Vec<UserCommand> {
     vec![UserCommand {
-        keys: vec!['A', 'B'],
+        keys: vec!['A', 'B'], 
         command: "S".to_string()
     }]
-
 }
 
-fn default_exec() -> Vec<String> { 
+fn default_exec() -> Vec<String> {
     vec!["L".to_string(), "O".to_string(), "L".to_string()]
 }
 
@@ -82,14 +90,11 @@ fn default_border_focus_color() -> String { "black".to_string() }
 fn default_titlebar() -> bool { false }
 fn default_gap() -> u8 { 3 }
 
-// fn load_config()
-
-fn load_default_config()
-
-
 fn main() {
-
+    
+    // Opens the config.yaml file.
     let f = File::open("./examples/config.yml").expect("Could not open file.");
+    // Reads the Values from the 'config' struct in config.yml 
     let user_config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
 
     println!("{:?}", user_config);
