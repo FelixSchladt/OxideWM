b3de993a8bab219342b8c79a080f36704faa832b
Felix Schladt <felix.schladt@hensoldt.net>
2022-12-25 14:06:40 +0100

    eventloop prototype

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,54 @@ pub mod config;
 use windowmanager::WindowManager;
 use std::error::Error;
 use x11rb::connection::Connection;
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
+use x11rb::rust_connection::RustConnection;
+use x11rb::protocol::Event;
+
+#[derive(Debug)]
+struct IpcEvent {
+    test: String,
+}
+
+
+fn dbus_ipc_loop(sender: Sender<IpcEvent>) {
+    loop {
+        sender.send(IpcEvent { test: "test".to_string() }).unwrap();
+        thread::sleep(std::time::Duration::from_millis(1000));
+    }
+}
 
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut manager = WindowManager::new();
 
+    let (sender, receiver) = channel();
+
+    thread::spawn(move || {
+        dbus_ipc_loop(sender);
+    });
+
+    loop {
+        /*
+        while let Some(event) = manager.connection.borrow().poll_for_event()? {
+            manager.handle_event(&event);
+        }*/
+        let event = manager.connection.borrow_mut().poll_for_event().unwrap();
+        match event {
+            Some(event) => manager.handle_event(&event),
+            None => (),
+        }
+
+
+        let ipc_event = receiver.try_recv();
+        match ipc_event {
+            Ok(event) => println!("Received IPC Event: {:?}", event),
+            Err(_) => (),
+        }
+    }
+
+    /*
     let mut event;
 
     loop {
@@ -25,7 +68,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
 
         manager.connection.borrow_mut().flush()?;
-    }
+    }*/
 
     Ok(())
 }
