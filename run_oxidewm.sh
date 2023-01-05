dd98b400748260e0c7638d288bfaf834e6f0ceeb
B1TC0R3 <github@b1tc0r3.com>
2023-01-05 16:28:48 +0100

    Add xephyr install

--- run_oxidewm.sh
+++ run_oxidewm.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo -e "\x1b[1m\x1b[36m#- Thank you for using OxideWM -#\x1b[0m"
+function install_xephyr () {
+    echo -ne " [\x1b[36m~\x1b[0m] Installing Xephyr...\n"\
+             "[\x1b[36m~\x1b[0m] Detecting package manager...\n"\
+             "[\x1b[32m+\x1b[0m] "
+
+    whereis apt    | grep -i "/apt"    && sudo apt install xserver-xephyr
+    whereis pacman | grep -i "/pacman" && sudo pacman -S xorg-server-xephyr
+
+    echo -e " [\x1b[32m+\x1b[0m] Installation complete"
+}
+
+echo -ne "\x1b[1m\x1b[31m#- Thank you for using OxideWM -#\x1b[0m\n"\
+         "[\x1b[36m~\x1b[0m] Checking whether Xephyr is installed...\n"\
+         "[\x1b[32m+\x1b[0m] "
+
+whereis Xephyr | grep "/Xephyr" && install_xephyr  # TODO: Change && to || after testing is done
