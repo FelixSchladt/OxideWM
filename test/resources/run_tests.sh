fecdfd35f7b87252d9415403da88cca284843eb3
B1TC0R3 <b1tc0r3@proton.me>
2023-02-06 16:57:01 +0100

    Refactor filenames

--- test/resources/run_tests.sh
+++ test/resources/run_tests.sh
@@ -3,12 +3,12 @@
 
 XEPHYR=$(whereis -b Xephyr | cut -f2 -d ' ')
 
-echo    "┏━━━━━━━━━━━┓"
-echo -e "┃ \x1b[32m\x1b[1mUNITTESTS\x1b[0m ┃"
-echo    "┗━━━━━━━━━━━┛"
+echo    "┏━━━━━━━━━━━━━━━━━━━┓"
+echo -e "┃ \x1b[32m\x1b[1mINTEGRATION TESTS\x1b[0m ┃"
+echo    "┗━━━━━━━━━━━━━━━━━━━┛"
 xinit ./test/resources/unittestrc -- $XEPHYR :100 -ac -screen 511x50 -host-cursor
 
 echo    "┏━━━━━━━━━━━━━━━━━┓"
 echo -e "┃ \x1b[32m\x1b[1mAUTOMATED TESTS\x1b[0m ┃"
 echo    "┗━━━━━━━━━━━━━━━━━┛"
-xinit ./test/resources/oxidemsgrc -- $XEPHYR :100 -ac -screen 1200x1000 -host-cursor
+xinit ./test/resources/automated_tests -- $XEPHYR :100 -ac -screen 1200x1000 -host-cursor
