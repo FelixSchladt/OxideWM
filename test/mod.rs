e7575058e38380ea9f09657c656549d124c5efbd
B1TC0R3 <b1tc0r3@proton.me>
2023-02-10 10:21:15 +0100

    Exclude some tests from github pipeline testrun

--- test/mod.rs
+++ test/mod.rs
@@ -1,2 +1,9 @@
+pub fn in_pipeline() -> bool {
+    match std::env::var("GITHUB_PIPELINE") {
+        Ok(_)  => true,
+        Err(_) => false,
+    }
+}
+
 pub mod config_tests;
 pub mod screeninfo_tests;
