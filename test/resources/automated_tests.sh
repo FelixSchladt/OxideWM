26dc4133df359e933ce7f6b3a9d59be2d65080fe
B1TC0R3 <b1tc0r3@proton.me>
2023-02-10 09:09:15 +0100

    Refactor test method

--- test/resources/automated_tests.sh
+++ test/resources/automated_tests.sh
@@ -4,23 +4,26 @@ function run_test() {
     # $3 = Success message
     # $4 = Failure message
     # $5 = Sleep duration, use when a oxide action takes some time to complete
-
+    cmd=$1
+    success_requirement=$2
+    success_message=$3
+    failure_message=$4
     counter=$5
 
-    echo -e "($5 s) Testing: '$1'   \t\x1b[A\x1b[K"
-    bash -c "$1"
+    echo -e "($counter s) Testing: '$cmd'   \t\x1b[A\x1b[K"
+    bash -c "$cmd"
 
     while [ $counter -gt 0 ]; do
         sleep 1
         counter=$(( $counter -1 ))
-        echo -e "($counter s) Testing: '$1'  \x1b[A\x1b[K"
+        echo -e "($counter s) Testing: '$cmd'  \x1b[A\x1b[K"
     done
 
     state=`./target/debug/oxide-msg -c state`
-    if grep -q -E $2 <<< $state; then
-        echo -e "\x1b[32m\x1b[1mTEST SUCCESS\x1b[0m - '$1' - $3"
+    if grep -q -E $success_requirement <<< $state; then
+        echo -e "\x1b[32m\x1b[1mTEST SUCCESS\x1b[0m - '$cmd' - $success_message"
     else
-        echo -e "\x1b[31m\x1b[1mTEST FAILED\x1b[0m  - '$1' - $4\n$state"
+        echo -e "\x1b[31m\x1b[1mTEST FAILED\x1b[0m  - '$cmd' - $failure_message\n$state"
     fi
 }
 
