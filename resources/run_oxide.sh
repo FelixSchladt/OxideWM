b593340fd58cf49104ea062836308704fb5bbada
Felix Schladt <82309717+FelixSchladt@users.noreply.github.com>
2023-03-30 15:58:34 +0200

    Update run_oxide.sh

--- resources/run_oxide.sh
+++ resources/run_oxide.sh
@@ -9,6 +9,9 @@ function start_application() {
 echo -e "[\x1b[36m~\x1b[0m] Building Oxide-bar..."
 cargo build -p oxide-bar
 
+echo -e "[\x1b[36m~\x1b[0m] Building Oxide-msg..."
+cargo build -p oxide-msg
+
 echo -e "[\x1b[36m~\x1b[0m] Building OxideWM..."
 cargo build && start_application
 
